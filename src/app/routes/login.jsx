5c4b3edf106f01c634df817a2ada9f0cc5dbe650
fiddleswe <smartsoodprasert69@gmail.com>
2025-08-29 23:36:19 +0700

    improved form attributes and changed design

--- src/app/routes/login.jsx
+++ src/app/routes/login.jsx
@@ -1,10 +1,14 @@
 function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="flex h-screen items-center justify-center flex-col">
+    <div className="flex h-screen flex-col items-center justify-center">
       <div className="h-[45vh] w-[45vh] overflow-hidden rounded-lg border bg-[#D3DAD9]/20 backdrop-blur-sm">
         <form
           className="flex h-full flex-col items-center"
-          onSubmit={(e) => e.preventDefault()}
+          onSubmit={handleSubmit}
         >
           <div className="flex w-full justify-center pt-7 text-2xl font-semibold">
             LOGIN
@@ -13,42 +17,50 @@ function Login() {
             htmlFor="email"
             className="text-md mt-7 mb-2 self-start pl-6 font-medium"
           >
-            Email Address
+            Email address
           </label>
           <input
             id="email"
+            type="email"
+            maxLength={30}
             placeholder="username@gmail.com"
             className="mb-5 h-11 w-[40vh] rounded-lg border border-black/40 bg-white pl-3 text-sm font-normal outline-none focus:ring-2 focus:ring-blue-500"
             required
           ></input>
-          <label
-            htmlFor="password"
-            className="text-md mb-2 self-start pl-6 font-medium"
-          >
-            Password
-          </label>
+          <div className="flex justify-between items-center w-full">
+            <label
+              htmlFor="password"
+              className="text-md mb-2 self-start pl-6 font-medium"
+            >
+              Password
+            </label>
+            <a href="#" className="text-md self-end pr-6 mb-2 font-normal text-[#1877F2] hover:underline">Forgot password?</a>
+          </div>
           <input
             id="password"
-            placeholder="password123 "
+            type="password"
+            maxLength={30}
             className="mb-9 h-11 w-[40vh] rounded-lg border border-black/40 bg-white pl-3 text-sm font-normal outline-none focus:ring-2 focus:ring-blue-500"
             required
           ></input>
           <button
             value="Submit"
             type="submit"
-            className="text-md h-11 w-[40vh] rounded-lg border border-black/40 bg-[#1877F2] font-normal text-white outline-none focus:ring-2 focus:ring-blue-500"
+            className="text-md h-11 w-[40vh] cursor-pointer rounded-lg border border-black/40 bg-[#1877F2] font-normal text-white outline-none focus:ring-2 focus:ring-blue-500"
           >
             Login
           </button>
         </form>
 
-        <div className="flex h-screen flex-col items-center justify-center">
-          <div className="h-[45vh] w-[45vh] overflow-hidden rounded-lg border bg-[#D3DAD9]/20 backdrop-blur-sm"></div>
-        </div>
       </div>
       <div className="mt-7 flex flex-col items-center justify-center">
         <div className="h-[10vh] w-[45vh] overflow-hidden rounded-lg border bg-[#D3DAD9]/20 backdrop-blur-sm">
-          <p className="justify-center items-center flex h-full">To Be Add</p>
+          <p className="flex h-full items-center justify-center">
+            Don't have account?&nbsp;
+            <a href="#" className="text-[#1877F2] hover:underline">
+              Sign up here!
+            </a>
+          </p>
         </div>
       </div>
     </div>
