072977034313826434eb61b3624c1aa4efbb2e30
fiddleswe <smartsoodprasert69@gmail.com>
2025-08-30 18:49:23 +0700

    updated path and react-router dom to synced the path

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./routes/login";
+import Signup from "./routes/signup";
+import Home from "./routes/home";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
       </Routes>
     </>
   );
