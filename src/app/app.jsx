31593e355918ae7022d3db154a2eedd1f37d74ed
fiddleswe <smartsoodprasert69@gmail.com>
2025-09-02 10:29:15 +0700

    updated: added more pages to react-router

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -2,15 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./routes/login";
 import Signup from "./routes/signup";
 import Home from "./routes/home";
+import Explore from "./routes/explore";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Signup" element={<Signup />} />
-      </Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Explore />} />
+        <Route path="/profile" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes> 
     </>
   );
 }
